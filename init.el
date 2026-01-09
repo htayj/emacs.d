cd7ccba8a47d152aebe0f63eeb7bc7413fd33c44
htayj <hardy.taylor.j@gmail.com>
2026-01-08 22:25:33 -0500

    make default font a little bit bigger

--- init.el
+++ init.el
@@ -598,6 +598,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#000000" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 142 :width normal :foundry "simp" :family "Hack")))))
 
 
